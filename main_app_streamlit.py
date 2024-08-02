05aacb4259d72070f62fcbd0844a3e58c711d4c1
ShivAppmocx <shivam.t@appmocx.com>
2024-08-02 17:54:19 +0530

    resolved scrolling and date format issue

--- main_app_streamlit.py
+++ main_app_streamlit.py
@@ -57,31 +57,44 @@ if user_message:
         # Run the SQL query and display the results
         sql_results = query_database(query_like, conn)
         # st.write("Results:")
-        # st.dataframe(sql_results)
+        st.dataframe(sql_results)
 
+        # Assuming sql_results is a DataFrame containing your query results
         df = pd.DataFrame(sql_results)
-        #  # Configure AgGrid options
-        gb = GridOptionsBuilder.from_dataframe(df)
-        is_fit_column = True
-        if "PURCHASE ORDER NUMBER" in sql_results.columns:
-            is_fit_column = False
-            gb.configure_column("PURCHASE ORDER NUMBER", pinned="left")  # Freeze this column
-        
-        cell_style = {'textAlign': 'center'}
-        # header_style = {'textAlign': 'left'}
-        # gb.configure_default_column(resizable=True, wrapText=True, autoHeight=True,  cellStyle=cell_style, headerStyle=header_style)
-        gb.configure_default_column(resizable=True, wrapText=True, autoHeight=True,  cellStyle=cell_style)
+        df.insert(0, 'Sr.No', range(1, len(df) + 1))
 
-        # Add custom CSS for header alignment
-       
-        gb.configure_grid_options(domLayout='autoHeight')
+       # Detect columns with datetime-like strings and convert their format
+        for col in df.columns:
+            if df[col].dtype == 'object':
+                try:
+                    df[col] = pd.to_datetime(df[col], format='%Y-%m-%dT%H:%M:%S.%f')
+                    df[col] = df[col].dt.strftime('%Y-%m-%d')  # Adjust format as needed
+                except ValueError:
+                    # If conversion fails, skip this column
+                    pass
+
+
+        # Configure AgGrid options
+        gb = GridOptionsBuilder.from_dataframe(df)
+        gb.configure_column("Sr.No", pinned="left")  # Freeze this column
+        # is_fit_column = True
+        is_fit_column = len(df.columns) <= 4  # Set is_fit_column based on column count
+
+        if "PURCHASE ORDER NUMBER" in df.columns:
+            # is_fit_column = False
+            gb.configure_column("PURCHASE ORDER NUMBER", pinned="left")  # Freeze this column
+
+        cell_style = {'textAlign': 'center'}
+        gb.configure_default_column(resizable=True, wrapText=True, autoHeight=True, cellStyle=cell_style)
+
+        # Ensure the scrollbar is visible
+        gb.configure_grid_options(domLayout='normal')
 
         gridOptions = gb.build()
 
         # Display the DataFrame with AgGrid
         st.write("Results:")
         AgGrid(df, gridOptions=gridOptions, fit_columns_on_grid_load=is_fit_column, enable_enterprise_modules=True)
-
-
+    
     except Exception as e:
         st.write(f"An error occurred: {e}")
